a059758c5d681456fe14b866f7bcdb632b520613
seandinwiddie <seandinwiddie@charter.net>
2017-06-10 17:01:45 -0700

    Create digital-currency.php

---
+++ digital-currency.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package WP Digital Currency
+ * @version 1.0
+ */
+/*
+Plugin Name: WP Digital Currency
+Plugin URI: https://wordpress.org/plugins/wp-digital-currency/
+Description: A WordPress plugin to display digital currency market prices.
+Author: ~
+Version: 1.0
+Author URI: #
+Text Domain: wp-digital-currency
+*/
+
+?>
